ea2a8983dcfbd53ac423ddd9d9b9b4e3b2f1a7ce
Daniel Vitor <114309972+DanielVFF@users.noreply.github.com>
2024-11-08 14:14:26 -0300

    feature: Definição de modulo para ORM e UserRepo
    Esse modulo deve ficar dentro da pasta de infraestrutura, repositórios devem ficar todos aqui para facilitar manutenção e ajustes de lógica

---
+++ api-gateway-auth/src/infrastructure/prisma/repositories/user.repository.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/infrastructure/prisma/prisma.service';
+import { User, Prisma } from '@prisma/client';
+import { IUserRepository } from 'src/interfaces/user_repository.interface';
+
+@Injectable()
+export class UserRepository implements IUserRepository {
+  constructor(private prisma: PrismaService) {}
+
+  async getUserById(user_id: string): Promise<User | null> {
+    return await this.prisma.user.findUnique({ where: { user_id } });
+  }
+
+  async getAllUsers(): Promise<User[]> {
+    return await this.prisma.user.findMany();
+  }
+
+  async createUser(data: Prisma.UserCreateInput): Promise<User> {
+    return await this.prisma.user.create({ data });
+  }
+
+  async updateUser(user_id: string, data: Partial<Prisma.UserUpdateInput>): Promise<User> {
+    return await this.prisma.user.update({
+      where: { user_id },
+      data,
+    });
+  }
+
+  async deleteUser(user_id: string): Promise<User> {
+    return await this.prisma.user.delete({
+      where: { user_id },
+    });
+  }
+}
