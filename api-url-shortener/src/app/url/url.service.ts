27e86b95bf548941c85cfb45a63a7680c946b681
Daniel Vitor <114309972+DanielVFF@users.noreply.github.com>
2024-11-09 16:10:55 -0300

    chore: REfatorando modulo de url
    Basicamente adaptando para aceitar parâmetros com nomes genéricos e adaptando para seguir mais a regra de negocio esperada

--- api-url-shortener/src/app/url/url.service.ts
+++ api-url-shortener/src/app/url/url.service.ts
@@ -1,22 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import { UrlRepository } from 'src/infrastructure/prisma/repositories/url.repository';
 import { Url, Prisma } from '@prisma/client';
+import { CreateUrlDto } from './dto/create-url.dto';
+import { HelpersService } from 'src/infrastructure/helpers/helpers.service';
+import { DeleteUrlDto } from './dto/delete-url.dto';
 
 @Injectable()
 export class UrlService {
-  constructor(private readonly urlRepository: UrlRepository) {}
+  constructor(private readonly urlRepository: UrlRepository, private readonly helperService: HelpersService) {}
 
-  async createUrl(data: Prisma.UrlCreateInput): Promise<Url> {
-    return await this.urlRepository.createUrl(data);
+  async createUrl(data: CreateUrlDto): Promise<Url> {
+    if(!data?.short_url){
+      let isUnique : boolean = false;
+      let short_url : string;
+      while(!isUnique){
+        short_url = this.helperService.generateUrl()
+        const url = await this.urlRepository.getUrlByShortUrl(short_url);
+        if(!url){
+          isUnique = true;
+        }
+      }
+      data.short_url = short_url
+    }
+    return await this.urlRepository.createUrl(data as Prisma.UrlCreateInput );
   }
 
-  async getUrlById(url_id: string): Promise<Url | null> {
-    return await this.urlRepository.getUrlById(url_id);
+  async getUrlByUserId(user_id: string): Promise<Url[] | null> {
+    return await this.urlRepository.getUrlByUserId(user_id);
   }
 
-  async getAllUrls(): Promise<Url[]> {
-    return await this.urlRepository.getAllUrls();
-  }
 
   async updateUrl(
     url_id: string,
@@ -25,15 +37,17 @@ export class UrlService {
     return await this.urlRepository.updateUrl(url_id, data);
   }
 
-  async deleteUrl(url_id: string): Promise<Url> {
-    return await this.urlRepository.deleteUrl(url_id);
+  async deleteUrl(data : DeleteUrlDto): Promise<Url> {
+    return await this.urlRepository.deleteUrl(data);
   }
 
   async getUrlByShortUrl(short_url: string): Promise<Url | null> {
-    return await this.urlRepository.getUrlByShortUrl(short_url);
-  }
+    const url =  await this.urlRepository.getUrlByShortUrl(short_url);
 
-  async restoreUrl(url_id: string): Promise<Url> {
-    return await this.urlRepository.restoreUrl(url_id);
+    await this.urlRepository.updateUrl(url?.url_id,{
+      click_count : url?.click_count+1
+    })
+
+    return url;
   }
 }
