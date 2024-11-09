fa61aeba3d9fd3fe9dd30faffb6caa0fe8a76630
Daniel Vitor <114309972+DanielVFF@users.noreply.github.com>
2024-11-09 16:09:39 -0300

    chore: Removendo função de teste

--- api-url-shortener/src/app/app.controller.ts
+++ api-url-shortener/src/app/app.controller.ts
@@ -1,10 +1,5 @@
 import { Controller } from '@nestjs/common';
-import { MessagePattern } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
-  @MessagePattern({ cmd: 'process_data' })
-  handleMessage(data: any) {
-    return { response: 'Message processed' };
-  }
 }
