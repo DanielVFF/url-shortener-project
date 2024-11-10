54b11e2042a19c634155a3bed44cf55c58dcc6e7
Daniel Vitor <114309972+DanielVFF@users.noreply.github.com>
2024-11-10 19:22:42 -0300

    chore: Atualizando testes

--- api-url-shortener/src/app/url/url.controller.spec.ts
+++ api-url-shortener/src/app/url/url.controller.spec.ts
@@ -3,39 +3,27 @@ import { UrlController } from './url.controller';
 import { UrlService } from './url.service';
 import { Url } from '@prisma/client';
 import { CreateUrlInterface } from 'src/interfaces/url/create-url.interface';
-import { SearchByUserUrlInteface } from 'src/interfaces/url/search-by-user-url.interface';
 import { UpdateUrlInterface } from 'src/interfaces/url/update-url.interface';
+import { SearchByUserUrlInteface } from 'src/interfaces/url/search-by-user-url.interface';
 import { SearchByShortUrlInteface } from 'src/interfaces/url/search-by-short-url.interface';
 
 describe('UrlController', () => {
   let controller: UrlController;
   let service: UrlService;
 
-  const mockUrl: Url = {
-    url_id: '1',
-    original_url: 'https://example.com',
-    short_url: 'abcd1234',
-    user_id: 'user1',
-    click_count: 0,
-    status: 0,
-    created_at: new Date(),
-    deleted_at: new Date(),
+  const mockUrlService = {
+    createUrl: jest.fn(),
+    getUrlByUserId: jest.fn(),
+    updateUrl: jest.fn(),
+    deleteUrl: jest.fn(),
+    getUrlByShortUrl: jest.fn(),
   };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UrlController],
       providers: [
-        {
-          provide: UrlService,
-          useValue: {
-            createUrl: jest.fn().mockResolvedValue(mockUrl),
-            getUrlByUserId: jest.fn().mockResolvedValue([mockUrl]),
-            updateUrl: jest.fn().mockResolvedValue(mockUrl),
-            deleteUrl: jest.fn().mockResolvedValue(mockUrl),
-            getUrlByShortUrl: jest.fn().mockResolvedValue(mockUrl),
-          },
-        },
+        { provide: UrlService, useValue: mockUrlService },
       ],
     }).compile();
 
@@ -43,64 +31,135 @@ describe('UrlController', () => {
     service = module.get<UrlService>(UrlService);
   });
 
-  it('deve estar definido', () => {
-    expect(controller).toBeDefined();
+  it('deve criar uma URL', async () => {
+    const createUrlPayload: CreateUrlInterface = {
+      original_url: 'http://teste.com',
+      user_id: 'user123',
+    };
+
+    const createdUrl: Url = {
+      url_id: 'some-id',
+      original_url: 'http://teste.com',
+      short_url: '123',
+      click_count: 0,
+      created_at: new Date(),
+      status: 1,
+      updated_at: new Date(),
+      deleted_at: null,
+      user_id: 'user123',
+    };
+
+    jest.spyOn(service, 'createUrl').mockResolvedValue(createdUrl);
+
+    expect(await controller.createUrl(createUrlPayload)).toEqual(createdUrl);
   });
 
-  describe('createUrl', () => {
-    it('deve chamar o service.createUrl com os dados corretos e retornar o resultado', async () => {
-      const data: CreateUrlInterface = {
-        original_url: 'https://example.com',
-        user_id: 'user1',
-      };
-      const result = await controller.createUrl(data);
-      expect(service.createUrl).toHaveBeenCalledWith(data);
-      expect(result).toEqual(mockUrl);
-    });
+  it('deve obter URLs por user_id', async () => {
+    const getUrlByUserIdPayload: SearchByUserUrlInteface = {
+      user_id: 'user123',
+    };
+
+    const urls: Url[] = [
+      {
+        url_id: 'some-id',
+        original_url: 'http://teste.com',
+        short_url: '123',
+        click_count: 10,
+        created_at: new Date(),
+        status: 1,
+        updated_at: new Date(),
+        deleted_at: null,
+        user_id: 'user123',
+      },
+      {
+        url_id: 'id123',
+        original_url: 'http://teste.com',
+        short_url: '123',
+        click_count: 5,
+        created_at: new Date(),
+        status: 1,
+        updated_at: new Date(),
+        deleted_at: null,
+        user_id: 'user123',
+      },
+    ];
+
+    jest.spyOn(service, 'getUrlByUserId').mockResolvedValue(urls);
+
+    expect(await controller.getUrlById(getUrlByUserIdPayload)).toEqual(urls);
   });
 
-  describe('getUrlById', () => {
-    it('deve chamar o service.getUrlByUserId com o user_id correto e retornar o resultado', async () => {
-      const data: SearchByUserUrlInteface = { user_id: 'user1' };
-      const result = await controller.getUrlById(data);
-      expect(service.getUrlByUserId).toHaveBeenCalledWith(data.user_id);
-      expect(result).toEqual([mockUrl]);
-    });
+  it('deve atualizar uma URL', async () => {
+    const updateUrlPayload: UpdateUrlInterface = {
+      url_id: '1234',
+      data: {
+        original_url: 'http://teste.com',
+        short_url: '123',
+        click_count: 100,
+        status: 1,
+        user_id: 'user123',
+      }
+    };
+
+    const updatedUrl: Url = {
+      url_id: '1234',
+      original_url: 'http://teste.com',
+      short_url: '123',
+      click_count: 100,
+      created_at: new Date(),
+      status: 1,
+      updated_at: new Date(),
+      deleted_at: null,
+      user_id: 'user123',
+    };
+
+    jest.spyOn(service, 'updateUrl').mockResolvedValue(updatedUrl);
+
+    expect(await controller.updateUrl(updateUrlPayload)).toEqual(updatedUrl);
   });
 
-  describe('updateUrl', () => {
-    it('deve chamar o service.updateUrl com os dados corretos e retornar o resultado', async () => {
-      const data: UpdateUrlInterface = {
-        url_id: '1',
-        data: { original_url: 'https://updated.com' },
-      };
-      const result = await controller.updateUrl(data);
-      expect(service.updateUrl).toHaveBeenCalledWith(data.url_id, data.data);
-      expect(result).toEqual(mockUrl);
-    });
+  it('deve excluir uma URL', async () => {
+    const deleteUrlPayload = {
+      url_id: '1234',
+      user_id: 'user123',
+    };
+
+    const deletedUrl: Url = {
+      url_id: '1234',
+      original_url: 'http://teste.com',
+      short_url: '123',
+      click_count: 0,
+      created_at: new Date(),
+      status: 0,
+      updated_at: new Date(),
+      deleted_at: new Date(),
+      user_id: 'user123',
+    };
+
+    jest.spyOn(service, 'deleteUrl').mockResolvedValue(deletedUrl);
+
+    expect(await controller.deleteUrl(deleteUrlPayload)).toEqual(deletedUrl);
   });
 
-  describe('deleteUrl', () => {
-    it('deve chamar o service.deleteUrl com os dados corretos e retornar o resultado', async () => {
-      const data: SearchByShortUrlInteface & SearchByUserUrlInteface = {
-        short_url: 'abcd1234',
-        user_id: 'user1',
-      };
-      const result = await controller.deleteUrl(data);
-      expect(service.deleteUrl).toHaveBeenCalledWith({
-        short_url: data.short_url,
-        user_id: data.user_id,
-      });
-      expect(result).toEqual(mockUrl);
-    });
-  });
+  it('deve obter uma URL por short URL', async () => {
+    const getUrlByShortUrlPayload: SearchByShortUrlInteface = {
+      short_url: '123',
+    };
 
-  describe('getUrlByShortUrl', () => {
-    it('deve chamar o service.getUrlByShortUrl com o short_url correto e retornar o resultado', async () => {
-      const data: SearchByShortUrlInteface = { short_url: 'abcd1234' };
-      const result = await controller.getUrlByShortUrl(data);
-      expect(service.getUrlByShortUrl).toHaveBeenCalledWith(data.short_url);
-      expect(result).toEqual(mockUrl);
-    });
+    const url: Url = {
+      url_id: 'some-id',
+      original_url: 'http://teste.com',
+      short_url: '123',
+      click_count: 0,
+      created_at: new Date(),
+      status: 1,
+      updated_at: new Date(),
+      deleted_at: null,
+      user_id: 'user123',
+    };
+
+    jest.spyOn(service, 'getUrlByShortUrl').mockResolvedValue(url);
+
+    expect(await controller.getUrlByShortUrl(getUrlByShortUrlPayload)).toEqual(url);
   });
 });
