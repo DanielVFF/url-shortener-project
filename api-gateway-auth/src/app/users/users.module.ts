aaae8195c2f694d385476b72ed673b759ff2a289
Daniel Vitor <114309972+DanielVFF@users.noreply.github.com>
2024-11-10 01:07:10 -0300

    fix: Adicionando importe necessário para guards

--- api-gateway-auth/src/app/users/users.module.ts
+++ api-gateway-auth/src/app/users/users.module.ts
@@ -4,8 +4,9 @@ import { UsersService } from './users.service';
 import { PrismaService } from 'src/infrastructure/prisma/prisma.service';
 import { UserRepository } from 'src/infrastructure/prisma/repositories/user.repository';
 import { HelpersService } from 'src/infrastructure/helpers/helpers.service';
+import { JwtService } from '@nestjs/jwt';
 @Module({
   controllers: [UsersController],
-  providers: [UsersService, PrismaService, HelpersService, UserRepository],
+  providers: [UsersService, PrismaService, HelpersService, UserRepository, JwtService],
 })
 export class UsersModule {}
